5859f086961cabe3ee9618d321fddf0947eb2125
ulysses-299 <u.a.g141999@gmail.com>
2025-08-10 10:47:08 -0600

    Editamos el encabezado y el input se reinicia al agregar un amigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    
+    limpiarCaja();
+    console.log(amigo);
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+asignarTextoElemento('h2', 'Escriba el nombre de sus amigos.');
